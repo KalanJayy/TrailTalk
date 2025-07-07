dd6c50da6b5f6685a818d17eb2c5f1b36b17af77
Kalana Jayaweera <kalana33471@gmail.com>
2025-07-07 22:39:02 +0530

    backend developing

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/register", async (req,res) => {
+    res.send("register");
+});
+
+router.post("/login", async (req,res) => {
+    res.send("login");
+});
+
+
+
+export default router;
