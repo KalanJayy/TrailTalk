181810711a7e929a95f32e70245abea6d2d37da1
Kalana Jayaweera <kalana33471@gmail.com>
2025-07-08 23:37:18 +0530

    updated user

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schmea({
-    email:{
-        
-    }
+    username:{type:String, required:true, unique:true},
+    email:{type: String, required: true, unique:true}
 });
