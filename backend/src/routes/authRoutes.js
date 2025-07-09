9f24f5c327e475371494c7542df49d2b703871c3
Kalana Jayaweera <kalana33471@gmail.com>
2025-07-09 14:43:22 +0530

    updating server

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,7 +3,11 @@ import express from "express";
 const router = express.Router();
 
 router.post("/register", async (req,res) => {
-    res.send("register");
+    try {
+        res.send("register");
+    } catch (error) {
+        
+    }
 });
 
 router.post("/login", async (req,res) => {
