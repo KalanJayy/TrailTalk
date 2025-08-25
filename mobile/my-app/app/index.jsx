4c5c3b984272684ec49d11bf2d491365c1696da0
Kalana <kalana33471@gmail.com>
2025-08-25 21:40:31 +0530

    Login signup added

--- mobile/my-app/app/index.jsx
+++ mobile/my-app/app/index.jsx
@@ -1,16 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
- 
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {color:"blue"}
+});
